febadabdb2574d8e6f43a1fde42cb9b2233b9ad3
Muklis12133 <muklis6763@gmail.com>
2022-09-28 10:26:23 +0700

    Create hay.js

---
+++ plugins/hay.js
@@ -0,0 +1,17 @@
+import fs from'fs'
+let { MessageType } = (await import('@adiwajshing/baileys')).default
+let handler = async (m, { conn }) => {
+let whmods = fs.readFileSync('./tmp/hay.opus') 
+conn.sendFile(m.chat, whmods, '', '', m, true)
+}
+
+handler.customPrefix = /^(bot|robot|p|hallo|hello|tes|tes123|muklis|klis|woy)$/i
+handler.command = new RegExp
+
+handler.limit = true
+handler.mods = false 
+handler.premium = false
+handler.group = false
+handler.private = false
+
+export default handler
